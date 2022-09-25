2d7bab9faec3be922d86d837bcaf9c220537d73b
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2022-09-25 19:51:18 -0300

    feat: criando carrosel

---
+++ challenge-17-kenai/src/javascript/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    slidesPerView: 2,
+
+  });
